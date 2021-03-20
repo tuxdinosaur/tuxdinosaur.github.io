a2454de65ae42f6c0fb7fd433704b4ed83ba84c8
tuxdinosaur <ivan.tux@outlook.com>
2021-03-20 17:46:04 -0600

    :zap: [UPDATE] Se agregó posible lista de colores de acento

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 
+/* COLORS 
+  - #C72B6C
+  - #FF3864
+  - #3581B8
+  - #B6244F
+  - #7C77B9
+  - #9D8189
+  - #D65780
+  - #9792E3
+  - #FBAF00
+  - #DB5461
+  - #7E7F9A
+  - #BA2C73
+  - #1B998B
+  - #119DA4
+  - #EF798A
+*/
+
 var arrColors = ["#333333", "#1B998B"]
 var numColor = Math.random()*2
 console.log(numColor)
