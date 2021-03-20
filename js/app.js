efd3e6c93ed15afa6cfb525e4f185e0441e3d9b7
tuxdinosaur <ivan.tux@outlook.com>
2021-03-20 16:21:15 -0600

    :zap: [UPDATE] Se agregó ejemplo para cambiar color de acento en el sitio de forma aleatoria

---
+++ js/app.js
@@ -0,0 +1,8 @@
+
+var arrColors = ["#333333", "#1B998B"]
+var numColor = Math.random()*2
+console.log(numColor)
+
+let floorNumColor = Math.floor(numColor)
+console.log(floorNumColor)
+console.log(arrColors[floorNumColor])
